d812efd4598711cd5042191b30b2b040555d8cb5
Nicolas Rocha Lacerda <nrochalacerda@gmail.com>
2025-02-21 15:36:28 -0300

    added gitignore
    added gitignore

--- ORGHub-Gateway/Program.cs
+++ ORGHub-Gateway/Program.cs
@@ -1,6 +1,9 @@
+using ORGHub_Gateway.Factories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<ApiFactory>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
